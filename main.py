36eee625bd5faa9b09447d25325605988c684585
yaronprigal <yaronprigal@microsoft.com>
2022-12-06 17:24:15 +0200

    fix

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ container_audience = "https://containerregistry.azure.net"
 cred = ManagedIdentityCredential()
 aad_access_token = cred.get_token(container_audience)
 
-os.environ.set("OUTPUT_TOKEN", aad_access_token.token)
+os.environ["OUTPUT_TOKEN"] = aad_access_token.token
