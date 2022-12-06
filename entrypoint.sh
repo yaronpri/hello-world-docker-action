b1debb0be0e32995b56bed4a263454da1ee45e26
yaronprigal <yaronprigal@microsoft.com>
2022-12-06 17:37:38 +0200

    s

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 pwd
 ls -l
 python3 /main.py
-printenv
+var1 = $OUTPUT_TOKEN
+curl --header 'application/x-www-form-urlencoded' --request POST --data 'grant_type=access_token&service=$1&access_token=$var1' https://$1/oauth2/exchange
