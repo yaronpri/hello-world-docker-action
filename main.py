c124f08f2d2b7017608eaa4b02ff722f85dee213
yaronprigal <yaronprigal@microsoft.com>
2022-12-06 16:19:17 +0200

    remove env

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import os
 from azure.identity import ManagedIdentityCredential
 from json import loads
 
-acr_uri = os.environ.get("ACR_URI", "")
-tenant_id = os.environ.get("TENANT_ID", "")
-
 audience = "https://management.azure.com"
 container_audience = "https://containerregistry.azure.net"
 
