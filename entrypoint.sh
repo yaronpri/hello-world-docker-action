2e1a670a4d238c84ec19453a5c93978f8546004b
yaronprigal <yaronprigal@microsoft.com>
2022-12-07 09:52:09 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -l
-pwd
-ls -l
 python3 /main.py
-var1 = $OUTPUT_TOKEN
-curl --header 'application/x-www-form-urlencoded' --request POST --data 'grant_type=access_token&service=$1&access_token=$var1' https://$1/oauth2/exchange
+echo "Success exec main.py"
+curl --header 'application/x-www-form-urlencoded' --request POST --data 'grant_type=access_token&service=$1&access_token=ssss' https://$1/oauth2/exchange
