4105f07d717e39b7db0ecb1e7d951bde3f21810c
yaronprigal <yaronprigal@microsoft.com>
2022-12-11 15:06:17 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ acruri=$(echo "$1")
 clientid=$(echo "$2")
 tenantid=$(echo "$3")
 echo $acruri
-aadtoken=$(python3 main.py)
+aadtoken=$(python3 /main.py)
 data="grant_type=access_token&service=testmanagedyp.azurecr.io&access_token=$aadtoken"
 result=$(curl --header 'application/x-www-form-urlencoded' -s --request POST --data $data https://testmanagedyp.azurecr.io/oauth2/exchange | jq -r '.refresh_token')
 echo $result
