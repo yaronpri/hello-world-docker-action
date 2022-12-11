5e2d835a42ff10014cbac91891da67eb815422e1
yaronprigal <yaronprigal@microsoft.com>
2022-12-11 15:56:39 +0200

    fix concat

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ fi
 data="grant_type=access_token&service=$acruri&access_token=$aadtoken"
 if ! [ -z "$tenantid" ]
 then
-  data=data + "&tenant=$tenantid"
+  data="$data&tenant=$tenantid"
 fi 
 url="https://$acruri/oauth2/exchange"
 result=$(curl --header 'application/x-www-form-urlencoded' -s --request POST --data $data $url | jq -r '.refresh_token')
