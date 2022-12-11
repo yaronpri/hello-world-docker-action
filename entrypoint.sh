3ae949319fb3ec7bf9a30586b09e9733646b07cb
yaronprigal <yaronprigal@microsoft.com>
2022-12-11 15:16:00 +0200

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,17 @@
 acruri=$(echo "$1")
 clientid=$(echo "$2")
 tenantid=$(echo "$3")
-echo $acruri
-aadtoken=$(python3 /main.py)
-data="grant_type=access_token&service=testmanagedyp.azurecr.io&access_token=$aadtoken"
-result=$(curl --header 'application/x-www-form-urlencoded' -s --request POST --data $data https://testmanagedyp.azurecr.io/oauth2/exchange | jq -r '.refresh_token')
+if [ -z "$clientid" ]
+then
+  aadtoken=$(python3 /main.py)
+else
+  aadtoken=$(python3 /main.py $clientid)
+fi
+data="grant_type=access_token&service=$acruri&access_token=$aadtoken"
+if ![ -z "$tenantid" ]
+then
+  data=data + "&tenant=$tenantid"
+fi 
+url="https://$acruri/oauth2/exchange"
+result=$(curl --header 'application/x-www-form-urlencoded' -s --request POST --data $data $url | jq -r '.refresh_token')
 echo $result
